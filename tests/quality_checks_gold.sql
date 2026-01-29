dd357b357abc3bf80459a7417286d2d244696c4e
markisabelle <mark.isabelle14@gmail.com>
2026-01-29 12:23:57 -0500

    Update quality_checks_gold.sql

--- tests/quality_checks_gold.sql
+++ tests/quality_checks_gold.sql
@@ -42,10 +42,10 @@ HAVING COUNT(*) > 1;
 -- Checking 'gold.fact_sales'
 -- ====================================================================
 -- Check the data model connectivity between fact and dimensions
-SELECT * 
-FROM gold.fact_sales f
-LEFT JOIN gold.dim_customers c
+SELECT *
+FROM gold.fact_sales f 
+LEFT JOIN gold.dim_customers c 
 ON c.customer_key = f.customer_key
-LEFT JOIN gold.dim_products p
+LEFT JOIN gold.dim_products p 
 ON p.product_key = f.product_key
-WHERE p.product_key IS NULL OR c.customer_key IS NULL 
+WHERE p.product_key IS NULL OR c.customer_key IS NULL
